16692a109e552c38091c5a480c60307fb7a4764b
amirreza vafaeenezhad <amirreza.vafaeenzhad@gmail.com>
2024-05-23 13:37:12 -0700

    Add changes to the galaxy shooter project

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float _SpeedUp = 8f;
+
+    [SerializeField] private GameObject _LaserPrefab;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.up * _SpeedUp * Time.deltaTime);
+        if (transform.position.y > 7)
+        {
+            Destroy(this.GameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float sppeed = 1;
+    [SerializeField]
+    private float _sppeed = 1;
+    [SerializeField]
+    private GameObject _LaserPrefab;
+    
     void Start()
     {
         transform.position = new Vector3(1, 1, 1);
@@ -14,16 +18,20 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculatMovement();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_LaserPrefab , transform.position, Quaternion.identity);
+        }
     }
 
-    void CalculatMovement();
+    void CalculatMovement()
     {
         float horizontalOutput = Input.GetAxis("Horizontal");
         float verticalOutput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3 (horizontalOutput, verticalOutput, 0);
         
-        transform.Translate(  direction * sppeed * Time.deltaTime);
-        if (transform.position.y >= 0)
+        transform.Translate(  direction * _sppeed * Time.deltaTime);
+        if ( transform.position.y >= 0)
         {
             transform.position = new Vector3(transform.position.x, 0, 0);
         }
