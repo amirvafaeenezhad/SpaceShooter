260c6acddb65d8bd4af7564cacce247783f720c8
amirreza vafaeenezhad <amirreza.vafaeenzhad@gmail.com>
2024-05-29 16:02:50 -0700

    Add details to enemy script (adding random spawn behavior)
    is not complete yet

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private float _speedDown = 0.4f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,15 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y <7 && transform.position.y>-7 )
+        {
+            transform.Translate(Vector3.down* _speedDown* Time.deltaTime);
+        }
+        else
+        {
+            int RandomX = Random.Range(-11, 11);
+            Instantiate(RandomX, 7, Quaternion.identity);
+        }
         
     }
 }
